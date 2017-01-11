19ac436883168788e486ca4cfa3fe2e290275c17
Derek Chopp <djchopp@mtu.edu>
2017-01-11 16:27:08 -0500

    Remove long folder name and first tutorial

--- djchopp/InstallingandConfiguringROSEnvironment/catkin_ws/build/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/ros/EE5900/EE5900-Proj1-Group4/djchopp/InstallingandConfiguringROSEnvironment/catkin_ws/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
