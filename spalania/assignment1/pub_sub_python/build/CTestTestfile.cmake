8742136884020156bc993918e4a7c46392091992
mano <spalania@mtu.edu>
2017-01-15 21:27:57 -0600

    Build the catkin workspace.

---
+++ spalania/assignment1/pub_sub_python/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/mano/EE5900-Proj1-Group4/spalania/assignment1/pub_sub_python/src
+# Build directory: /home/mano/EE5900-Proj1-Group4/spalania/assignment1/pub_sub_python/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
---
+++ spalania/assignment1/pub_sub_python/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/mano/EE5900-Proj1-Group4/spalania/assignment1/pub_sub_python/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/mano/catkin_ws/devel;/opt/ros/indigo".split(';') if "/home/mano/catkin_ws/devel;/opt/ros/indigo" != "" else []
