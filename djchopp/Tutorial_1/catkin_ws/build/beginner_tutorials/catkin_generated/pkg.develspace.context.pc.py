bb44643c8c04e082679f7d7925bd07ed80ec48c4
Derek Chopp <djchopp@mtu.edu>
2017-01-16 14:57:31 -0500

    Starting fresh on dereks tutorials

--- djchopp/Tutorial_1/catkin_ws/build/beginner_tutorials/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "beginner_tutorials"
-PROJECT_SPACE_DIR = "/home/ros/EE5900/EE5900-Proj1-Group4/djchopp/Tutorial_1/catkin_ws/devel"
-PROJECT_VERSION = "0.0.0"
