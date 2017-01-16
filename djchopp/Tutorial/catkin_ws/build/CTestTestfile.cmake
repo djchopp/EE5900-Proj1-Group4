fc99738cfcfd4ec998e0e8e6d49f430334544319
Derek Chopp <djchopp@mtu.edu>
2017-01-16 15:02:51 -0500

    Finished building workspace

---
+++ djchopp/Tutorial/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ros/EE5900_Repos/EE5900-Proj1-Group4/djchopp/Tutorial/catkin_ws/src
+# Build directory: /home/ros/EE5900_Repos/EE5900-Proj1-Group4/djchopp/Tutorial/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
