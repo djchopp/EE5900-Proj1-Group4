c5d30f130c050bad4ebe4cd42fab0ab84d0c22db
mano <spalania@mtu.edu>
2017-01-17 13:26:38 -0600

    Complete final program with all basic functionalities.

--- spalania/assignment1/pub_sub_python/src/pub_sub/scripts/publisher.py
+++ spalania/assignment1/pub_sub_python/src/pub_sub/scripts/publisher.py
@@ -3,6 +3,8 @@
 import rospy
 import sys
 from std_msgs.msg import String
+
+# import the custom definied message
 from pub_sub.msg import message
 
 # import openweathermap library
@@ -11,7 +13,6 @@ import pyowm
 # talker function is called at the beginning of the program
 def talker():
 	# create a ros node with the topic information and the type of the message.	
-	
 	pub = rospy.Publisher('chatter', message, queue_size=10)
 	# initiate the node in the name of talker
 	rospy.init_node('talker', anonymous=True)
@@ -31,13 +32,16 @@ def talker():
 		# example: 'rosrun pub_sub publisher.py 49931 US'
 		observation = owm.weather_at_zip_code(sys.argv[1], sys.argv[2])
 		w = observation.get_weather()
+		# load all the parameters defined in the message.msg file
+		# (temperature, wind, humidity, status)
 		weather = message()
+		# convert temperature from kelvin to celsius
 		temper = w.get_temperature('celsius')
 		weather.temperature = temper['temp']
 		win = w.get_wind()
 		weather.wind = win['speed']
 		weather.humidity = w.get_humidity()
-		weather.status = w.get_status()
+		weather.status = w.get_detailed_status()
 		
 		# message to publish
 		# send_str = "Temperature: %f" % temperature['temp']
--- spalania/assignment1/pub_sub_python/src/pub_sub/scripts/subscriber.py
+++ spalania/assignment1/pub_sub_python/src/pub_sub/scripts/subscriber.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import String
+
+# import the custom definied message
 from pub_sub.msg import message
 
 def callback(message):
@@ -9,12 +11,12 @@ def callback(message):
 	print('The wind speed: {} km/s').format(message.wind)
 	print('The humidity: {}').format(message.humidity)
 	print('Staus: %s') % message.status
+
 def listener():
 	# initiate the listener node in the name of listener.
 	rospy.init_node('listener', anonymous=True)
 	# subscribe to the topic chatter as defined in the publisher program 
 	rospy.Subscriber("chatter", message, callback)
-
 	rospy.spin()
 
 if __name__ == '__main__':
