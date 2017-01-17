a4500c4c5d562dc80bb72c001b79efb665db1c96
mano <spalania@mtu.edu>
2017-01-16 22:42:39 -0600

    Complete task: Query weather data for a preset city.

--- spalania/assignment1/pub_sub_python/src/pub_sub/scripts/subscriber.py
+++ spalania/assignment1/pub_sub_python/src/pub_sub/scripts/subscriber.py
@@ -3,11 +3,13 @@ import rospy
 from std_msgs.msg import String
 
 def callback(data):
+	# message subscribed from the publisher
 	rospy.loginfo(rospy.get_caller_id() + "I received %s", data.data)
 
 def listener():
-
+	# initiate the listener node in the name of listener.
 	rospy.init_node('listener', anonymous=True)
+	# subscribe to the topic chatter as defined in the publisher program 
 	rospy.Subscriber("chatter", String, callback)
 
 	rospy.spin()
