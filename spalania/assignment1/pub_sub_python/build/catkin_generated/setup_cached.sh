0210600b5bbf316d5105a678c5cde8160c19a3b0
mano <spalania@mtu.edu>
2017-01-17 05:35:38 -0600

    Add basic layout for the GUI.

--- spalania/assignment1/pub_sub_python/build/catkin_generated/setup_cached.sh
+++ spalania/assignment1/pub_sub_python/build/catkin_generated/setup_cached.sh
@@ -7,9 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/mano/EE5900-Proj1-Group4/spalania/assignment1/pub_sub_python/devel:/opt/ros/indigo"
-export LD_LIBRARY_PATH="/home/mano/EE5900-Proj1-Group4/spalania/assignment1/pub_sub_python/devel/lib:/opt/ros/indigo/lib"
-export PKG_CONFIG_PATH="/home/mano/EE5900-Proj1-Group4/spalania/assignment1/pub_sub_python/devel/lib/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
 export PWD="/home/mano/EE5900-Proj1-Group4/spalania/assignment1/pub_sub_python/build"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/mano/EE5900-Proj1-Group4/spalania/assignment1/pub_sub_python/devel/share/common-lisp"
-export ROS_PACKAGE_PATH="/home/mano/EE5900-Proj1-Group4/spalania/assignment1/pub_sub_python/src:/opt/ros/indigo/share:/opt/ros/indigo/stacks"
--- spalania/assignment1/pub_sub_python/src/pub_sub/scripts/publisher.py
+++ spalania/assignment1/pub_sub_python/src/pub_sub/scripts/publisher.py
@@ -2,26 +2,76 @@
 
 import rospy
 from std_msgs.msg import String
+
+# import openweathermap library
 import pyowm
 
-def talker():
-	pub = rospy.Publisher('chatter', String, queue_size=10)
-	rospy.init_node('talker', anonymous=True)
-	rate = rospy.Rate(0.1)
-	while not rospy.is_shutdown():
-		owm = pyowm.OWM('29a84d545476863693ae58a97a49182b')
-		observation = owm.weather_at_place('Chennai, in')
-		w = observation.get_weather()
+# import GUI library
+import Tkinter as tk
 
+city = 'Chennai, in'
+# api key from openweathermap website
+API_key = '29a84d545476863693ae58a97a49182b'
+
+# talker function is called at the beginning of the program
+def talker():
+	# create a ros node with the topic information and the type of the message.
+	pub = rospy.Publisher('chatter', String, queue_size=10)
+	# initiate the node in the name of talker
+	rospy.init_node('talker', anonymous=True)
+
+	# rate at which the publisher publishes the weather info on the screen.
+	# 0.1 Hz ==> 1 message per ten second
+	rate = rospy.Rate(0.1)
+
+	# import the API key
+	owm = pyowm.OWM(API_key)
+
+	# basic while, which will run untill the publisher node is closed
+	loop = 0
+	while not rospy.is_shutdown():
+		# weather info about the specified place
+		observation = owm.weather_at_place(city)
+		w = observation.get_weather()
 		temperature = w.get_temperature('celsius')
-		send_str = "Temperature: %d" % temperature['temp']
+		
+		# message to publish
+		send_str = "Temperature: %f" % temperature['temp']
 		rospy.loginfo(send_str)
 		pub.publish(send_str)
 		rate.sleep()
 
+
+def gui_func():
+	top_layer = tk.Tk()
+
+
+	top_frame = tk.Frame(top_layer)
+	top_frame.pack()
+
+	middle_frame = tk.Frame(top_layer)
+	middle_frame.pack()
+
+	bottom_frame = tk.Frame(top_layer)
+	bottom_frame.pack(side = tk.BOTTOM)
+
+	label1 = tk.Label(top_frame, text='Enter the zip code: ')
+	label1.pack(side=tk.LEFT)
+	entry1 = tk.Entry(top_frame, bd=5)
+	entry1.pack(side=tk.RIGHT)
+
+	label2 = tk.Label(middle_frame, text='Enter the country intialis: ')
+	label2.pack(side=tk.LEFT)
+	entry2 = tk.Entry(middle_frame, bd=5)
+	entry2.pack(side=tk.RIGHT)
+
+	button = tk.Button(bottom_frame, text='query', command=talker)
+	button.pack()
+
+	top_layer.mainloop()
+
 if __name__ == '__main__':
 	try:
-		talker()
+		gui_func()
 	except rospy.ROSInterruptException:
 		pass
-
